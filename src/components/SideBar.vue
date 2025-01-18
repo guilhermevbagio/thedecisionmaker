6b6333f5d0be27c1ab9be05500567a9927c9e0e7
Guilherme <guilhermevbagio@gmail.com>
2025-01-18 11:35:30 -0300

    fix tailwind, start sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="h-full w-64 bg-gray-800 text-white">
-    <p class="p-4 bg-gray-600">THE DECISION MAKER</p>
+  <div
+    class="h-full w-64 border-[#AD4B57] rounded-lg rounded-l-none border-2 bg-[#3C1E2F] flex flex-col"
+  >
+    <header class="p-4 border-b-2 border-[#AD4B57] w-full text-[#f8EAEE] tracking-tighter">
+      THE DECISION MAKER.
+    </header>
+
+    <ul>
+      <li class="p-4 h-fit w-full text-[#f8EAEE] tracking-tighter">Home</li>
+      <li class="p-4 h-fit w-full text-[#f8EAEE] tracking-tighter">About</li>
+      <li class="p-4 h-fit w-full text-[#f8EAEE] tracking-tighter">Contact</li>
+    </ul>
   </div>
 </template>
