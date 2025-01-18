df502cb4ab327a55c0fd5840864ff593992d1648
Guilherme <guilhermevbagio@gmail.com>
2025-01-18 14:02:36 -0300

    welcome page kinda unstyled

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="h-screen w-screen flex flex-row">
+  <div v-if="welcomed" class="h-screen w-screen flex flex-row">
     <SideBar />
+    <div class="p-4">
+      <router-view></router-view>
+    </div>
   </div>
+  <WelcomeView v-else @open="welcomed = true"></WelcomeView>
 </template>
 
 <script setup lang="ts">
 import SideBar from './components/SideBar.vue'
+import WelcomeView from './views/WelcomeView.vue'
+import { ref } from 'vue'
+
+const welcomed = ref(false)
 </script>
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-screen w-screen flex flex-col justify-center">
+    <h1 class="text-4xl text-center font-bold">The world isn't fair</h1>
+    <h2 class="text-2xl text-center">but maybe you should be</h2>
+
+    <button
+      class="mt-10 bg-[#AD4B57] text-[#f8EAEE] p-4 w-fit self-center rounded"
+      @click="emits('open')"
+    >
+      Make a decision
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emits = defineEmits(['open'])
+</script>
